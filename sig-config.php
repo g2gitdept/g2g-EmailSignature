1177e29b9e0f31ee137f229c482a1d5be42f78bc
g2gitdept <44876403+g2gitdept@users.noreply.github.com>
2024-10-22 14:44:11 -0400

    justify div to center & comment out markdown button

--- sig-config.php
+++ sig-config.php
@@ -26,12 +26,14 @@
     }
    
 </style>
-<div class="iframe_holder">
-    <iframe id="signature-iframe" style="width:500px;height:240px;border:none;" src="https://emailsignature.good2grow.com/sig.php?e_email=<?php echo $_GET['e_email']; ?>&e_topper=<?php echo $_GET['e_topper']; ?>&e_name=<?php echo $_GET['e_name']; ?>&e_title=<?php echo $_GET['e_title']; ?>&e_phone=<?php echo $_GET['e_phone']; ?>"></iframe>
-</div>
-<div>
-    <a class="button" onclick="copyIframeContent()">Copy Signature <i class="fa-solid fa-signature"></i></a>
-    <a class="button" onclick="copyIframeHTML()">Copy Markdown <i class="fa-solid fa-code"></i></a>
+<div style="display:flex;flex-direction:column;justify-self:center;align-items:center;">
+    <div class="iframe_holder">
+        <iframe id="signature-iframe" style="width:500px;height:240px;border:none;" src="https://emailsignature.good2grow.com/sig.php?e_email=<?php echo $_GET['e_email']; ?>&e_topper=<?php echo $_GET['e_topper']; ?>&e_name=<?php echo $_GET['e_name']; ?>&e_title=<?php echo $_GET['e_title']; ?>&e_phone=<?php echo $_GET['e_phone']; ?>"></iframe>
+    </div>
+    <div>
+        <a class="button" onclick="copyIframeContent()">Copy Signature <i class="fa-solid fa-signature"></i></a>
+        <!--<a class="button" onclick="copyIframeHTML()">Copy Markdown <i class="fa-solid fa-code"></i></a>-->
+    </div>
 </div>
 <script>
     function copyIframeContent() {
@@ -62,19 +64,19 @@
     selection.removeAllRanges();
 }
 
-function copyIframeHTML() {
-    var iframe = document.getElementById('signature-iframe');
-    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+//function copyIframeHTML() {
+//    var iframe = document.getElementById('signature-iframe');
+//    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-    var htmlContent = iframeDoc.documentElement.outerHTML;
+//    var htmlContent = iframeDoc.documentElement.outerHTML;
+//
+//    var tempTextarea = document.createElement('textarea');
+//    tempTextarea.value = htmlContent;
+//    document.body.appendChild(tempTextarea);
+//    tempTextarea.select();
+//    document.execCommand('copy');
+//    document.body.removeChild(tempTextarea);
 
-    var tempTextarea = document.createElement('textarea');
-    tempTextarea.value = htmlContent;
-    document.body.appendChild(tempTextarea);
-    tempTextarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempTextarea);
-
-        alert('Signature Markdown content copied to clipboard!');
-   }
+//        alert('Signature Markdown content copied to clipboard!');
+//   }
 </script>
